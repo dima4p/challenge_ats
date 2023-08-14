83e7553c42c68395da3b6642cfa335e89073e1a2
Dmitry Kulikov <dima@koulikoff.ru>
2023-08-14 20:13:13 +0200

    Generator configs are added

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,10 @@ module ApplicationTrackingSystem
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.colorize_logging = true
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
